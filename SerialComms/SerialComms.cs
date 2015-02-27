40376943b24e6595e57a164a531e12069c44c85c
Jeff <jeffmac710@hotmail.com>
2015-02-27 10:09:43 -0700

    First commit, working interface to com ports

---
+++ SerialComms/SerialComms.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+
+namespace SerialComms
+{
+    public class SerialComms
+    {
+        protected SerialPort comPort;
+
+        public delegate void Message(String message);
+        public event Message msg;
+
+        public delegate void recv(object data);
+        public event recv received;
+
+        public SerialComms()
+        {
+            init();
+        }
+
+        ~SerialComms()
+        {
+            comPort.Close();
+        }
+
+        public int init()
+        {
+            int status = -1;
+            comPort = new SerialPort();
+            comPort.DataReceived += new SerialDataReceivedEventHandler(dataReceived);
+            comPort.ErrorReceived += new SerialErrorReceivedEventHandler(errorReceived);
+            return status;
+        }
+
+        #region Getters and Setters
+
+        public int getBaudRate()
+        {
+            return comPort.BaudRate;
+        }
+
+        public int setBaudRate(int baud)
+        {
+            int status = -1;
+            comPort.BaudRate = baud;
+            return status;
+        }
+
+        public int getDataBits()
+        {
+            return comPort.DataBits;
+        }
+
+        public int setDataBits(int bits)
+        {
+            int status = -1;
+            comPort.DataBits = bits;
+            return status;
+        }
+
+        public StopBits getStopBits()
+        {
+            return comPort.StopBits;
+        }
+
+        public int setStopBits(StopBits bits)
+        {
+            int status = -1;
+            comPort.StopBits = bits;
+            return status;
+        }
+
+        public int setStopBits(String stopBits)
+        {
+            int status = -1;
+            setStopBits((StopBits)Enum.Parse(typeof(StopBits), stopBits));
+            return status;
+        }
+
+        public Parity getParity()
+        {
+            return comPort.Parity;
+        }
+
+        public int setParity(Parity parity)
+        {
+            int status = -1;
+            comPort.Parity = parity;
+            return status;
+        }
+
+        public int setParity(String parity)
+        {
+            int status = -1;
+            setParity((Parity)Enum.Parse(typeof(Parity), parity));
+            return status;
+        }
+
+        public String getPortName()
+        {
+            return comPort.PortName;
+        }
+
+        public int setPortName(String port)
+        {
+            int status = -1;
+            try
+            {
+                comPort.PortName = port;
+                status = 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid port name.", ex.Message);
+                if (msg != null)
+                    msg("Invalid port name.");
+            }
+            return status;
+        }
+
+        public bool isOpen()
+        {
+            bool status = false;
+            if (comPort != null)
+                status = comPort.IsOpen;
+            return status;
+        }
+
+        public static String[] getComPorts()
+        {
+            return SerialPort.GetPortNames();
+        }
+
+        public static String[] getParityValues()
+        {
+            return Enum.GetNames(typeof(Parity));
+        }
+
+        public static String[] getStopBitValues()
+        {
+            return Enum.GetNames(typeof(StopBits));
+        }
+
+        #endregion
+
+        public int open()
+        {
+            int status = -1;
+            try
+            {
+                //first check if the port is already open
+                //if its open then close it
+                if (comPort.IsOpen == true)
+                    comPort.Close();
+
+                //set the properties of our SerialPort Object
+                //comPort.BaudRate = getBaudRate();    //BaudRate
+                //comPort.DataBits = getDataBits();    //DataBits
+                //comPort.StopBits = getStopBits();    //StopBits
+                //comPort.Parity = getParity();    //Parity
+                //comPort.PortName = getPortName();   //PortName
+
+                //now open the port
+                comPort.Open();
+                status = 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failed to open port.", ex.Message);
+                if (msg != null)
+                    msg("Failed to open port.");
+            }
+            return status;
+        }
+
+        public int close()
+        {
+            int status = -1;
+            comPort.Close();
+            return status;
+        }
+
+        public int sendData(String data)
+        {
+            int status = -1;
+            //byte[] bytes = new byte[data.Length];
+            //for (int i = 0; i < data.Length; i++ )
+            //    bytes[i] = Convert.ToByte(data[i]);
+            //status = sendData(bytes);
+            try
+            {
+                comPort.Write(data);
+                status = 0;
+            }
+            catch (Exception e)
+            {
+                if (msg != null)
+                    msg(e.Message);
+            }
+            return status;
+        }
+
+        public int sendData(int data)
+        {
+            return sendData(BitConverter.GetBytes(data));
+        }
+
+        public int sendData(byte[] data)
+        {
+            int status = -1;
+            //send the message to the port
+            try
+            {
+                comPort.Write(data, 0, data.Length);
+                status = 0;
+            }
+            catch (Exception)
+            {
+            }
+            return status;
+        }
+
+        protected void dataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int numBytes = comPort.BytesToRead;
+            //create a byte array to hold the awaiting data
+            byte[] comBuffer = new byte[numBytes];
+            //read the data and store it
+            comPort.Read(comBuffer, 0, numBytes);
+
+            if (received != null)
+                received(comBuffer);
+        }
+
+        protected void errorReceived(object sender, SerialErrorReceivedEventArgs e)
+        {
+            if (msg != null)
+                msg(e.ToString());
+        }
+    }
+}
---
+++ SerialCommsTest/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SerialCommsTest
+{
+    class Program
+    {
+        public static int high = 0;
+
+        static void received(object data)
+        {
+            if (data is byte[])
+            {
+                //Console.Write("0x");
+                //int value = -1;
+                //foreach (byte b in (byte[])data)
+                //{
+                //    value = Convert.ToInt32(b);
+                //    value = value & 0x0F;
+                //    if (value > 7)
+                //        value = value >> 1;
+                //    string hexOutput = String.Format("{0:X}", value);
+                //    Console.Write(hexOutput);
+                //}
+                //Console.WriteLine();
+
+                String buffer = System.Text.Encoding.Default.GetString((byte[])data);
+                Console.WriteLine(buffer);
+                if (buffer.StartsWith("Buffer:"))
+                {
+                    buffer = buffer.Substring(7);
+                    Console.WriteLine(String.Format("{0:X}", buffer));
+                }
+
+                //Console.WriteLine("String: " + ASCIIEncoding.ASCII.GetString((byte[])data));
+                //if (value == high)
+                //    high++;
+                //if (high > 15)
+                //    high = 0;
+            }
+        }
+
+        static void consoleTest()
+        {
+            bool test = true;
+            int count = 0;
+            byte[] command = new byte[1];
+            command[0] = 0x01;
+
+            SerialComms.SerialComms comPort = new SerialComms.SerialComms();
+            comPort.setBaudRate(9600);    //BaudRate
+            comPort.setPortName("COM8");   //PortName
+            comPort.received += new SerialComms.SerialComms.recv(received);
+            comPort.open();
+            Thread.Sleep(10);
+            high = 1;
+            Console.WriteLine("High = " + high);
+            comPort.sendData(high + "\r\n");
+
+            while (test)
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    test = false;
+                }
+                Thread.Sleep(150);
+                if (count >= 5000f/150f)
+                {
+                    Console.WriteLine("High = " + high);
+                    comPort.sendData(high + "\r\n");
+                    //high++;
+                    //if (high >= 16)
+                    //    high = 0;
+                    count = 0;
+                }
+                count++;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            consoleTest();
+        }
+    }
+}
